928ad34a3e0941a3a8d93030f601643e2c1fdc29
Tefymahery <mihossiraman@gmail.com>
2024-12-10 14:29:08 +0300

    Test Material Tailwind 2

--- src/components/SimpleCard.js
+++ src/components/SimpleCard.js
@@ -1,22 +1,7 @@
 // components/SimpleCard.js
 import { Card, Button, Typography } from "@material-tailwind/react";
-import { useEffect, useState } from "react";
 
 export default function SimpleCard() {
-  const [isClient, setIsClient] = useState(false);
-
-  // Le useEffect s'exécute une seule fois après le premier rendu côté client
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setIsClient(true); // Le composant est rendu côté client
-    }
-  }, []); // Pas de dépendances, donc s'exécute une seule fois
-
-  // Retourner null si ce n'est pas côté client (éviter le rendu côté serveur)
-  if (!isClient) {
-    return null; // Empêche le rendu côté serveur
-  }
-
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <Card className="w-96 shadow-lg rounded-lg">
