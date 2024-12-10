3569f0b989e96534b08d4816ef0e4f33ae4b5ebb
Tefymahery <mihossiraman@gmail.com>
2024-12-10 14:23:38 +0300

    Test Material Tailwind

--- src/components/SimpleCard.js
+++ src/components/SimpleCard.js
@@ -1,17 +1,20 @@
 // components/SimpleCard.js
 import { Card, Button, Typography } from "@material-tailwind/react";
-
 import { useEffect, useState } from "react";
 
 export default function SimpleCard() {
   const [isClient, setIsClient] = useState(false);
 
+  // Le useEffect s'exécute une seule fois après le premier rendu côté client
   useEffect(() => {
-    setIsClient(true); // Le composant est rendu après le montage
-  }, []);
+    if (typeof window !== "undefined") {
+      setIsClient(true); // Le composant est rendu côté client
+    }
+  }, []); // Pas de dépendances, donc s'exécute une seule fois
 
+  // Retourner null si ce n'est pas côté client (éviter le rendu côté serveur)
   if (!isClient) {
-    return null; // Ne pas rendre le composant côté serveur
+    return null; // Empêche le rendu côté serveur
   }
 
   return (
@@ -19,7 +22,7 @@ export default function SimpleCard() {
       <Card className="w-96 shadow-lg rounded-lg">
         <div className="p-6">
           <Typography variant="h5" className="text-center mb-4">
-            Bienvenue sur mon site !
+            Bienvenue sur mon site be !
           </Typography>
           <Typography className="text-center mb-6">
             Ce composant est créé avec Material Tailwind. Il est simple et léger.
